64191127c971c76481231f1decd8893eabe30c8b
Paul Milbank <paulm@outlook.co.nz>
2019-02-28 22:08:08 +1300

    update the setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ANSIBLE_VER="2.7.8"
 
-apt-get install python-pip pythom3-pip
+apt-get install python-pip python3-pip software-properties-common
 
 pip3 install ansible==$ANSIBLE_VER --user
 
