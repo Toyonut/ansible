b961fa1a18da9ebfd5b03e8b28c6317743cad3d7
Paul Milbank <paulmilbank@gmail.com>
2020-02-18 21:24:39 +1300

    remove the ubuntu like flag.

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -4,4 +4,4 @@ sudo apt-get install python3-pip python3-distutils software-properties-common
 
 pip3 install --user -r ./requirements.txt
 
-ansible-playbook -K local-system-ubuntu.yml --extra-vars '{ "ubuntu_like":true }'
+ansible-playbook -K local-system-ubuntu.yml
