fa6eb244cb089bd6026f51c75ec3bdcc31b95e45
Paul Milbank <paulmilbank@gmail.com>
2020-04-26 20:33:13 +1200

    add reminder for exporting ansible path

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -5,3 +5,5 @@ sudo apt-get install python3-pip python3-distutils software-properties-common
 pip3 install --user -r ./requirements.txt
 
 ansible-playbook -K local-system-ubuntu.yml
+
+echo "You may need to add \"$HOME/.local/bin\" to your PATH to run ansible."
