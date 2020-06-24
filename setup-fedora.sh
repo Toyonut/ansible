0bd67e4d4941299ebcea3beefd2adb0176df0884
Paul Milbank <paul@pop-os.localdomain>
2020-06-24 21:31:03 +1200

    only echo message if Ansible fails

--- setup-fedora.sh
+++ setup-fedora.sh
@@ -3,3 +3,10 @@
 pip3 install --user -r ./requirements.txt
 
 ansible-playbook -K local-system-fedora.yml
+
+if [ $? != 0 ]
+then
+  echo "You may need to add \"$HOME/.local/bin\" to your PATH to run ansible."
+else
+  echo "Have a good day."
+fi
--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -6,4 +6,10 @@ pip3 install --user -r ./requirements.txt
 
 ansible-playbook -K local-system-ubuntu.yml
 
-echo "You may need to add \"$HOME/.local/bin\" to your PATH to run ansible."
+if [ $? != 0 ]
+then
+  echo "You may need to add \"$HOME/.local/bin\" to your PATH to run ansible."
+else
+  echo "Have a good day."
+fi
+
