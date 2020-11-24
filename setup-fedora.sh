0c661e9eed5dbb5610c15f23749ac27b38bc1e1c
Paul Milbank <paulmilbank@pop-os.localdomain>
2020-11-24 21:39:58 +1300

    Fix output that made no sense

--- setup-fedora.sh
+++ setup-fedora.sh
@@ -7,10 +7,10 @@ NEEDED_PATH="${HOME}/.local/bin"
 if [[ ${PATH} =~ ${NEEDED_PATH} ]]
 then
   echo "${HOME}/.local/bin is in PATH"
-  echo "Add ${HOME}/.local/bin to your environment PATH var to run ansible in future."
   ansible-playbook -K local-system-fedora.yml
 else
-  echo "${HOME}/.local/bin is in PATH"
+  echo "${HOME}/.local/bin is not in PATH"
+  echo "Add ${HOME}/.local/bin to your environment PATH var to run ansible in future."
   PATH="${PATH}:${HOME}/.local/bin" ansible-playbook -K local-system-fedora.yml
 fi
 
