160a1538764d88784b4d62bd75c05d6d8cd91242
Paul Milbank <paulmilbank@gmail.com>
2025-03-13 19:37:39 +1300

    Install Ansible-core and community general

--- setup-fedora.sh
+++ setup-fedora.sh
@@ -8,10 +8,12 @@ NEEDED_PATH="${HOME}/.local/bin"
 if [[ ${PATH} =~ ${NEEDED_PATH} ]]
 then
   echo "${HOME}/.local/bin is in PATH"
+  ansible-galaxy collection install community.general
   ansible-playbook -K local-system-fedora.yml
 else
   echo "${HOME}/.local/bin is not in PATH"
   echo "Add ${HOME}/.local/bin to your environment PATH var to run ansible in future."
+  PATH="${PATH}:${HOME}/.local/bin" ansible-galaxy collection install community.general
   PATH="${PATH}:${HOME}/.local/bin" ansible-playbook -K local-system-fedora.yml
 fi
 
